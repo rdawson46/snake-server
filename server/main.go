aa5bf0fda4ac24e6a34adfd1ce4bedf0247978eb
rdawson46 <dawson4623@gmail.com>
2024-10-07 21:12:19 -0400

    adding handling per connection

--- server/main.go
+++ server/main.go
@@ -18,6 +18,103 @@ IDEA:
  - send updates to all connections
 */
 
+func assert(msg string, assertions ...bool) {
+    for _, n := range assertions {
+        if !n {
+            fmt.Println(msg)
+            syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+        }
+    }
+}
+
+type connHandler func(string) (int, error)
+
+type Node struct {
+    value connHandler
+    next  *Node
+}
+
+type list struct {
+    head *Node
+    tail *Node
+    m    sync.Mutex
+}
+
+func newNode(c connHandler) *Node {
+    return &Node{
+        value: c,
+        next: nil,
+    }
+}
+
+func newList() list {
+    return list{
+        m: sync.Mutex{},
+        head: nil,
+        tail: nil,
+    }
+}
+
+func (l *list) append(c connHandler) {
+    n := newNode(c)
+
+    l.m.Lock()
+    defer l.m.Unlock()
+
+    if l.head == nil {
+        assert("tail isn't nil", l.tail != nil)
+
+        l.head = n
+        l.tail = n
+
+        return
+    } 
+
+    assert("tail is nil, head is not", l.tail != nil)
+    l.tail.next = n
+    l.tail = n
+}
+
+// rework with pointers, remove extra looping
+func (l *list) remove(i int) {
+    l.m.Lock()
+    defer l.m.Unlock()
+
+    if l.head == nil {
+        return
+    }
+
+    if i == 0 {
+        if l.head == l.tail {
+            l.tail = nil
+        }
+
+        l.head = l.head.next
+        return
+    }
+
+    var prev *Node = nil
+    curr := l.head
+    currIndex := 0
+
+    for currIndex != i && curr != nil {
+        prev = curr
+        curr = curr.next
+    }
+
+    if curr != nil {
+        return
+    }
+
+    assert("prev is nil", prev != nil)
+    prev.next = curr.next
+}
+
+func (l *list) cycle(s string) {
+    // loop through list and i.connHandler(s)
+    // if error remove at spot
+}
+
 type server struct {
     s        net.Listener
     shutdown chan struct{}
@@ -77,8 +174,8 @@ func (s *server) handleConnections() {
                 return
             }
 
+            // FIX: remove the go thread and adjust
             go s.handleConnection(conn)
-
         }
     }
 }
@@ -98,7 +195,11 @@ func (s *server) handleConnections() {
      - check if connection is still alive
          - if so, send out game status
 */
-func (s *server) handleConnection(c net.Conn) {}
+func (s *server) handleConnection(c net.Conn) connHandler {
+    return func(s string) (int, error) {
+        return c.Write([]byte(s))
+    }
+}
 
 func (s *server) listen() {
     defer s.wg.Done()
